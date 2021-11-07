5199913c3c309ecf07d6274fd89afb94b08d06d0
Muhamad Amirul <19381137+miesaf@users.noreply.github.com>
2021-11-07 13:41:44 +0800

    Enable wildcard permission

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -17,35 +17,41 @@ class PermissionSeeder extends Seeder
     public function run()
     {
         // create 'Permissions' permissions
-        Permission::create(['name' => 'list permissions', 'guard_name' => 'api']);
-        Permission::create(['name' => 'add permissions', 'guard_name' => 'api']);
-        Permission::create(['name' => 'update permissions', 'guard_name' => 'api']);
-        Permission::create(['name' => 'delete permissions', 'guard_name' => 'api']);
+        Permission::create(['name' => 'permissions.*', 'guard_name' => 'api']);
+        Permission::create(['name' => 'permissions.list', 'guard_name' => 'api']);
+        Permission::create(['name' => 'permissions.add', 'guard_name' => 'api']);
+        Permission::create(['name' => 'permissions.update', 'guard_name' => 'api']);
+        Permission::create(['name' => 'permissions.delete', 'guard_name' => 'api']);
 
         // create 'Roles' permissions
-        Permission::create(['name' => 'list roles', 'guard_name' => 'api']);
-        Permission::create(['name' => 'add roles', 'guard_name' => 'api']);
-        Permission::create(['name' => 'view roles', 'guard_name' => 'api']);
-        Permission::create(['name' => 'update roles', 'guard_name' => 'api']);
-        Permission::create(['name' => 'delete roles', 'guard_name' => 'api']);
+        Permission::create(['name' => 'roles.*', 'guard_name' => 'api']);
+        Permission::create(['name' => 'roles.list', 'guard_name' => 'api']);
+        Permission::create(['name' => 'roles.add', 'guard_name' => 'api']);
+        Permission::create(['name' => 'roles.view', 'guard_name' => 'api']);
+        Permission::create(['name' => 'roles.update', 'guard_name' => 'api']);
+        Permission::create(['name' => 'roles.delete', 'guard_name' => 'api']);
 
         // create 'Users' permissions
-        Permission::create(['name' => 'list users', 'guard_name' => 'api']);
-        Permission::create(['name' => 'add users', 'guard_name' => 'api']);
-        Permission::create(['name' => 'view users', 'guard_name' => 'api']);
-        Permission::create(['name' => 'update users', 'guard_name' => 'api']);
-        Permission::create(['name' => 'delete users', 'guard_name' => 'api']);
+        Permission::create(['name' => 'users.*', 'guard_name' => 'api']);
+        Permission::create(['name' => 'users.list', 'guard_name' => 'api']);
+        Permission::create(['name' => 'users.add', 'guard_name' => 'api']);
+        Permission::create(['name' => 'users.view', 'guard_name' => 'api']);
+        Permission::create(['name' => 'users.update', 'guard_name' => 'api']);
+        Permission::create(['name' => 'users.delete', 'guard_name' => 'api']);
 
         // create roles and assign existing permissions
         $role1 = Role::create(['name' => 'admin', 'guard_name' => 'api']);
         // gets all permissions via Gate::before rule; see AuthServiceProvider
+        $role1->givePermissionTo('permissions.*');
+        $role1->givePermissionTo('roles.*');
+        $role1->givePermissionTo('users.*');
 
         $role2 = Role::create(['name' => 'moderator', 'guard_name' => 'api']);
-        $role2->givePermissionTo('list users');
-        $role2->givePermissionTo('view users');
+        $role2->givePermissionTo('users.list');
+        $role2->givePermissionTo('users.view');
 
         $role3 = Role::create(['name' => 'user', 'guard_name' => 'api']);
-        $role3->givePermissionTo('list users');
+        $role3->givePermissionTo('users.list');
 
         // create demo users
         $user = User::factory()->create([
