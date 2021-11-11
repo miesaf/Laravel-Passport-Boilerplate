a244689d2de41920e384411082fb144bee6720d8
Muhamad Amirul <19381137+miesaf@users.noreply.github.com>
2021-11-11 17:29:02 +0800

    User resources

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use App\Http\Traits\ResponseTrait;
+use App\Models\User;
+use Auth;
+use Str;
+
+class UsersController extends Controller
+{
+    use ResponseTrait;
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        if(!Auth::user()->can('users.list')) {
+            return $this->forbidden();
+        }
+
+        if($users = User::all()) {
+            return $this->successWithData("Success", $users);
+        } else {
+            return $this->failure("Failed to list users");
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        if(!Auth::user()->can('users.add')) {
+            return $this->forbidden();
+        }
+
+        $validated = $request->validate([
+            'user_id' => 'required|unique:users',
+            'name' => 'required',
+            'email' => 'required|email',
+            'role' => 'exists:roles,name',
+            'permissions' => 'array|exists:permissions,name'
+        ]);
+
+        $validated['password'] = bcrypt(Str::random(8));
+
+        if(User::create($validated)->assignRole($request->role)->givePermissionTo($request->permissions)) {
+            return $this->success("User created successfully");
+        } else {
+            return $this->failure("Failed to create user");
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        if(!Auth::user()->can('users.view')) {
+            return $this->forbidden();
+        }
+
+        if($user = User::with('roles', 'roles.permissions')->with('permissions')->find($id)) {
+            return $this->successWithData("Success", $user);
+        } else {
+            return $this->failure("Failed to retrieve user");
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        if(!Auth::user()->can('users.update')) {
+            return $this->forbidden();
+        }
+
+        $validated = $request->validate([
+            'user_id' => 'required|unique:users,user_id,' . $id,
+            'name' => 'required',
+            'email' => 'required|email',
+            'role' => 'exists:roles,name',
+            'permissions' => 'array|exists:permissions,name'
+        ]);
+
+        if($user = User::find($id)) {
+            $user->update($validated);
+            $user->syncRoles($request->role);
+            $user->syncPermissions($request->permissions);
+
+            return $this->success("User updated successfully");
+        } else {
+            return $this->failure("Failed to update user");
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        if(!Auth::user()->can('users.delete')) {
+            return $this->forbidden();
+        }
+
+        if((User::find($id) != null) && User::find($id)->delete()) {
+            return $this->success("User deleted successfully");
+        } else {
+            return $this->failure("Failed to delete user");
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -49,4 +49,12 @@ Route::group(['middleware' => ['auth:api', 'forcePwdChg']], function () {
         Route::put('{id}', [RolesController::class, 'update']);
         Route::delete('{id}', [RolesController::class, 'destroy']);
     });
+
+    Route::group(['prefix' => 'users'], function () {
+        Route::get('/', [UsersController::class, 'index']);
+        Route::post('/', [UsersController::class, 'store']);
+        Route::get('{id}', [UsersController::class, 'show']);
+        Route::put('{id}', [UsersController::class, 'update']);
+        Route::delete('{id}', [UsersController::class, 'destroy']);
+    });
 });
