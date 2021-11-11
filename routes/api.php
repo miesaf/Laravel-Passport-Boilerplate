9f2e1910e1cf6ccfa2e38e141b6d0e9d58f915a8
Muhamad Amirul <19381137+miesaf@users.noreply.github.com>
2021-11-11 17:24:18 +0800

    Refresh token and change password

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Import your controllers
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\ProfilesController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +20,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', [LoginController::class, 'login'])->name('login');
+Route::post('refreshToken', [LoginController::class, 'refreshToken']);
+Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail']);
+Route::post('password/reset', [ResetPasswordController::class, 'reset']);
+
+Route::group(['middleware' => ['auth:api', 'forcePwdChg']], function () {
+    Route::group(['prefix' => 'user'], function () {
+        Route::get('/', function (Request $request) {
+            return $request->user();
+        })->withoutMiddleware('forcePwdChg');
+
+        Route::post('changePassword', [ProfilesController::class, 'changePassword'])->withoutMiddleware('forcePwdChg');
+    });
 });
