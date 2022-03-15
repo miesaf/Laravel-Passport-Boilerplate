12ffc2b55faab60c1ce3d9ba1bdfeedc118ee3f6
Muhamad Amirul <19381137+miesaf@users.noreply.github.com>
2022-03-15 18:01:14 +0800

    Response trait

--- app/Http/Traits/ResponseTrait.php
+++ app/Http/Traits/ResponseTrait.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Traits;
 
+use Illuminate\Support\Collection;
+
 trait ResponseTrait {
     protected function success($message, $status = 200)
     {
@@ -22,11 +24,53 @@ trait ResponseTrait {
         return response()->json($resData, $status);
     }
 
-    protected function failure($message, $status = 422)
+    protected function successWithCount($message, $data = [], $status = 200)
+    {
+        $count = 0;
+
+        if($data instanceof Collection) {
+            $count = $data->count();
+        } else if(is_array($data)) {
+            $count = count($data);
+        } else if(is_object($data)) {
+            $count = 1;
+        }
+
+        $resData = [
+            'status' => true,
+            'message' => $message,
+            'total' => $count,
+            'data' => $data
+        ];
+
+        return response()->json($resData, $status);
+    }
+
+    protected function successWithID($message, $id, $status = 200)
+    {
+        $resData = [
+            'status' => true,
+            'message' => $message,
+            'id' => $id
+        ];
+
+        return response()->json($resData, $status);
+    }
+
+    protected function failure($message, $status = 200)
     {
         return response()->json([
             'status' => false,
             'message' => $message,
+        ], $status);
+    }
+
+    protected function failedValidation($message, $errors, $status = 422)
+    {
+        return response()->json([
+            'status' => false,
+            'message' => $message,
+            'errors' => $errors
         ], $status);
     }
 
