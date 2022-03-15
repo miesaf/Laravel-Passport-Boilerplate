cf91b6c221efe54de3cccde444f6e7fe0720e7ad
Muhamad Amirul <19381137+miesaf@users.noreply.github.com>
2022-03-15 23:26:56 +0800

    Migration missing

---
+++ database/migrations/2022_02_07_143013_create_audit_trails_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuditTrailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('audit_trails', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->timestamp('req_time');
+            $table->text('vardata');
+            $table->string('category');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('audit_trails');
+    }
+}
