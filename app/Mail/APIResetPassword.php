6a447d22179a62a90d5399d51d853449e7a3248d
Muhamad Amirul <19381137+miesaf@users.noreply.github.com>
2022-04-03 15:16:43 +0800

    Pword reset mail function

---
+++ app/Mail/APIResetPassword.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class APIResetPassword extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $reset;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($reset)
+    {
+        $this->reset = $reset;
+    }
+
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject(config('app.name') . ' Password Reset')
+                    ->view('emails.passwordReset');
+
+    }
+}
